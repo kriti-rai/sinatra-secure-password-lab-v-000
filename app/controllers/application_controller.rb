49a33da480bcfdbb9a56c6231dd477a97a954ac1
Kriti Rai <kriti.rai@gmail.com>
2018-05-26 03:53:50 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -45,10 +45,12 @@ class ApplicationController < Sinatra::Base
       @user.save
     elsif !!params[:withdrawl]
       withdrawl_amount = params[:withdrawl]
+      @user.save
       if withdrawl_amount > @user.balance
         p "Not enough balance to make the withdrawl."
       else
         @user.balance = @user.balance - withdrawl_amount.to_f
+        @user.save
       end
     end
 
